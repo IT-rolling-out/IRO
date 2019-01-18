faa3e0150f68abdd6c2caaa4249a39ea5da89967
abarmal <yuram1inbox@gmail.com>
2019-01-18 02:37:05 +0200

    Finally... completely ready usefull version of build script and solution architecture.
    What will be in next commits (use git like note):
    - some fixes in CmdLine, FileIO;
    - maybe, update to netcore 3 (because of swashbuckle 5.0.0 beta)
    - webview;
    - unit tests for some old projects and its execution in build script.

--- build.ps1
+++ build.ps1
@@ -1,3 +1,7 @@
+###########################################################################
+# BASE FUNCTIONS
+###########################################################################
+
 function Write-Color([String[]]$Text, [ConsoleColor[]]$Color = "White", [int]$StartTab = 0, [int] $LinesBefore = 0,[int] $LinesAfter = 0, [string] $LogFile = "", $TimeFormat = "yyyy-MM-dd HH:mm:ss") {
     $DefaultColor = $Color[0]
     if ($LinesBefore -ne 0) {  for ($i = 0; $i -lt $LinesBefore; $i++) { Write-Host "`n" -NoNewline } } # Add empty line before
@@ -19,59 +23,47 @@ function Write-Color([String[]]$Text, [ConsoleColor[]]$Color = "White", [int]$St
     }
 }
 
-$Path="IRO.sln";
-$IsRelease="";
-$Configuration="";
+function CustomWrite([String[]]$Text, [ConsoleColor[]]$Color = "White"){
+  $Text=">>>>> "+$Text;
+  Write-Color -Text $Text -Color $Color;
+}
 
 function WriteOperationResultByExitCode($text, $exitCode)
 {    
-  if($exitCode -eq 0)
-  {
-    $text=$text+" Successful";	
-    Write-Color -Text $text -Color Green;	
-  }
-  else
+  if($exitCode)
   {
     $text=$text+" Failed";
-    Write-Color -Text $text -Color Red;
+    CustomWrite -Text $text -Color Red;
   }
-  $exitCodeInfo="Exit code is: "+ $exitCode;  
-  Write-Host $exitCodeInfo;
+  else
+  {    
+	$text=$text+" Successful";	
+    CustomWrite -Text $text -Color Green;	
+  }
   Write-Host "";
 }
 
 function ReadBool($hint)
 {
-  $hint=$hint+" (y/n) ";
+  $hint=$hint+" (y/n, y - default) ";
   $answer = Read-Host $hint
-  if($answer -eq 'y'){
-    return 1;
+  if($answer -eq 'n'){
+    return 0;
   }
-  return 0;
+  return 1;
 }
 
-function CleanSolution($path)
-{  
-  dotnet clean $path;
-  Write-Output $lastexitcode;
-  $exitCode=$lastexitcode;
-  WriteOperationResultByExitCode "Solution/Project clean status: "  $exitCode;
-  return $exitCode;
-}
+###########################################################################
+# CONFIGURATION
+###########################################################################
 
-function RestoreNugets($path)
-{
-  dotnet restore $path; 
-  $exitCode=$lastexitcode;
-  WriteOperationResultByExitCode "Nuget restore status: "  $exitCode;
-  return $exitCode;
-}
+$PSScriptRoot = Split-Path $MyInvocation.MyCommand.Path -Parent
 
 function AskConfiguration()
 {
-  $isRelease = ReadBool "Please enter 'y' if you wan't to build in release mode: "; 
-  $global:IsRelease=$isRelease;  
-  if($isRelease)
+  $global:IsRelease = ReadBool "Please enter 'y' if you wan't to build in release mode"; 
+  $global:IsRelease=$global:IsRelease;  
+  if($global:IsRelease)
   {
     $global:Configuration="Release";
   }
@@ -81,29 +73,41 @@ function AskConfiguration()
   }
 }
 
-function DotnetBuild($path)
+function DotnetBuildInfo()
 {     
-  dotnet build $path --configuration $global:Configuration
+  # dotnet build $path --configuration $global:Configuration
   $exitCode=$lastexitcode;
   Write-Host "Builded in " + $global:Configuration + " mode.";
   WriteOperationResultByExitCode "Dotnet build status: " $exitCode;
   return $exitCode;
 }
 
-$CleanExitStatus=CleanSolution $Path;
-pause;
+###########################################################################
+# EXECUTION
+###########################################################################
 
-$NugetRestoreExitStatus=RestoreNugets $Path;
-pause;
+$global:Path="$PSScriptRoot\IRO.sln";
+$global:IsRelease="";
+$global:Configuration="";
+
+$WantRemoveNupkgFromSrc = ReadBool "Want to remove all .nupkg files from '.\src' before build? ";
+if($WantRemoveNupkgFromSrc){
+  scripts\delete_nupkgs__src.cmd
+  CustomWrite "Removed .nupkg files." Green;
+  pause;  
+}
 
 AskConfiguration;
-$BuildExitStatus=DotnetBuild $path ;
-Write-Host '--------- BUILD SCRIPT FINISHED ---------';
-WriteOperationResultByExitCode "Solution/Project clean status: " $CleanExitStatus;
-WriteOperationResultByExitCode "Nuget restore status: " $NugetRestoreExitStatus;
-WriteOperationResultByExitCode "Dotnet build status: " $BuildExitStatus;
-$BuildScriptStatus= $CleanExitStatus+$NugetRestoreExitStatus+$BuildExitStatus;
-WriteOperationResultByExitCode "All script operations status: " $BuildExitStatus;
+dotnet build $global:Path --configuration $global:Configuration /clp:ErrorsOnly
+WriteOperationResultByExitCode "Solution build status: " $lastexitcode
 pause;
-exit $BuildScriptStatus;
-pause;
+
+$WantClearOutputNuget = ReadBool "Want clear '.\output\nupkg' and fill with new builded packages? "
+if($WantClearOutputNuget){
+  rd "$PSScriptRoot\output\nuget" -recurse;  
+  CustomWrite "Removed old." Green
+  pause;
+  scripts\copy_nupkgs.cmd "$PSScriptRoot\src\" "$PSScriptRoot\output\nuget\" $global:IsRelease 1
+  CustomWrite "Copied." Green
+}
+pause
